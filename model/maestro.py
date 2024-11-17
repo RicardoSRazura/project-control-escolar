ec0ed30cc523356c0a4776b78af80c4bc4b5d39f
Ricardo Razura <ricardorazest@gmail.com>
2024-11-16 21:38:24 -0600

    Añadido de interfaces faltantes

---
+++ model/maestro.py
@@ -0,0 +1,26 @@
+class Maestros():
+    def __init__(self, id_maestro=None, id_materia=None, id_usuario=None):
+        self.id_maestro = id_maestro
+        self.id_materia = id_materia
+        self.id_usuario = id_usuario
+        
+
+    def getId_maestro(self):
+        return self.id_maestro
+    
+    def setId_maestro(self, id_maestro):
+        self.id_maestro = id_maestro
+
+    def getId_materia(self):
+        return self.id_materia
+    
+    def setId_materia(self, id_materia):
+        self.id_materia= id_materia
+
+    def getId_usuario(self):
+        return self.usuario_id
+    
+    def setId_usuario(self, usuario_id):
+        self.usuario_id = usuario_id
+
+    
